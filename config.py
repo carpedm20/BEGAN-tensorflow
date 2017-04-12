4bfc283b7f4ea964c9aef27f0fbe9007a02f15b8
Taehoon Kim <carpedm20@gmail.com>
2017-04-13 03:53:15 +0900

    remove --use_author_code from config. fix #4

--- config.py
+++ config.py
@@ -19,7 +19,6 @@ net_arg.add_argument('--input_scale_size', type=int, default=64,
 net_arg.add_argument('--conv_hidden_num', type=int, default=128,
                      choices=[64, 128],help='n in the paper')
 net_arg.add_argument('--z_num', type=int, default=64, choices=[64, 128])
-net_arg.add_argument('--use_authors_model', type=str2bool, default=False)
 
 # Data
 data_arg = add_argument_group('Data')
