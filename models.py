57f8d35c8db31b60d5d494d9e21d96fc1941a049
Dominic Cheng <chengdazhi971014@gmail.com>
2017-04-22 23:44:53 +0800

    fix param type bug in python 3
    method numpy.prod returns a numpy.int64 value, which will cause an ValueError in python3. Refer to the following code block in layers.py$fully_connected()
    
    if not isinstance(num_outputs, six.integer_types):
        raise ValueError('num_outputs should be int or long, got %s.', num_outputs)
    
    and int() method converts bumpy.int64 to be compatible with six.integer_types

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ slim = tf.contrib.slim
 
 def GeneratorCNN(z, hidden_num, output_num, repeat_num, data_format, reuse):
     with tf.variable_scope("G", reuse=reuse) as vs:
-        x = slim.fully_connected(z, np.prod([8, 8, hidden_num]), activation_fn=None)
+        output_num = int(np.prod([8, 8, hidden_num]))
+        x = slim.fully_connected(z, output_num, activation_fn=None)
         x = reshape(x, 8, 8, hidden_num, data_format)
         
         for idx in range(repeat_num):
@@ -36,7 +37,8 @@ def DiscriminatorCNN(x, input_channel, z_num, repeat_num, hidden_num, data_forma
         z = x = slim.fully_connected(x, z_num, activation_fn=None)
 
         # Decoder
-        x = slim.fully_connected(x, np.prod([8, 8, hidden_num]), activation_fn=None)
+        output_num = int(np.prod([8, 8, hidden_num]))
+        x = slim.fully_connected(x, output_num, activation_fn=None)
         x = reshape(x, 8, 8, hidden_num, data_format)
         
         for idx in range(repeat_num):
